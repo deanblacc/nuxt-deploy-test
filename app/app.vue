523a5307b545cb99f26aad69bb13b2a1fdf43460
Dean <deanblacc@github.com>
2025-12-15 17:22:06 +0000

    init commit

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hero">
+    <img src="~/assets/images/logo.svg" alt="Logo" class="logo" />
+    <h1>Welcome to Nuxt</h1>
+    <p>Your minimal Nuxt application is ready to go!</p>
+    <a href="https://nuxt.com/docs" target="_blank" class="btn">
+      Read the Docs
+    </a>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  app: {
+    baseURL: '/nuxt-deploy-test/',
+    head: {
+      title: 'My Nuxt App',
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt-deploy-test/favicon.svg' }
+      ]
+    }
+  },
+
+  css: ['~/assets/css/main.css']
+})
